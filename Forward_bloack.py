124f009d9ce993f44479342df512dae294d3db5a
shripadpate20 <shripadpate20@gmail.com>
2024-08-30 19:47:47 +0530

    add FeedForwardBlock

---
+++ Forward_bloack.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model, d_ff, dropout):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff) 
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model) 
+
+    def forward(self, x):
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
